03095cf7a5c9953b64a0bbc9ca79673f36bde664
Darya Melgunova <darya.melgunova@gmail.com>
2025-02-25 21:01:01 +0300

    first

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,62 +1,54 @@
 package tests;
 
+import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
 
-public class SearchTests extends TestBase{
-    @Test
-    @DisplayName("Проверка возврата из поиска с помощью кнопки 'Назад'")
-    void backButtonFromSearchTest() {
-        step("Открываем поиск", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-        });
-
-        step("Вводим поисковой запрос", () -> {
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
-        });
-
-        step("Нажимаем кнопку 'Назад'", () -> {
-            $(id("org.wikipedia.alpha:id/search_close_btn")).click();
-        });
-
-        step("Проверяем, что поиск закрылся", () -> {
-            $(id("org.wikipedia.alpha:id/search_src_text")).shouldNotBe(visible);
-        });
-    }
-    @Test
-    @DisplayName("Проверка поиска без результатов")
-    void emptySearchResultsTest() {
-        step("Поиск по несуществующему запросу", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("wegaergqergqergqer");
-        });
-
-        step("Проверяем, что нет найденных статей", () ->
-                $(id("org.wikipedia.alpha:id/page_list_item_description")).shouldHave(text("No results")));
-
-    }
-    @Test
-    @DisplayName("Открытие первой найденной статьи и проверка заголовка")
-    void openFirstArticleAndCheckTitleTest() {
-        step("Выполняем поиск", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Selenium");
-        });
-
-        step("Открываем первую найденную статью", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title")).first().click()
-        );
-
-        step("Проверяем, что заголовок статьи содержит нужное слово", () ->
-                $(className("android.widget.TextView")).shouldHave(text("Selenium"))
-        );
-    }
-
+public class SearchTests extends TestBase {
+@Test
+@Tag("android")
+@DisplayName("Successful search")
+void successfulSearchTest() {
+    step("Type search", () -> {
+        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+    });
+    step("Verify content found", () ->
+            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container"))
+                    .shouldHave(sizeGreaterThan(0)));
 }
+
+@Test
+@Tag("android")
+@DisplayName("Open article from Main page")
+void openArticleFromMainPageTest() {
+    step("Click on article header ", () ->
+            $(AppiumBy.id("org.wikipedia.alpha:id/horizontal_scroll_list_item_text")).click());
+    step("Check opening", () ->
+            $$(AppiumBy.id("org.wikipedia.alpha:id/view_news_fullscreen_link_card_list"))
+                    .shouldHave(sizeGreaterThan(0)));
+}
+
+@Test
+@Tag("android")
+@DisplayName("Open article from Search")
+void openArticleFromSearchTest() {
+    step("Type search", () -> {
+        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("java");
+    });
+    step("Open first article", () ->
+            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container")).first().click());
+    step("Check opening", () ->
+            $(AppiumBy.id("org.wikipedia.alpha:id/view_wiki_error_text"))
+                    .shouldHave(text("An error occurred")));
+}
+}
+
